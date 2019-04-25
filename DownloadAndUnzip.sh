9a8af9685ec0c6ae84f67ab4f69e6399bcc72b76
Wonjik Kim <44762643+Likarian@users.noreply.github.com>
2019-04-25 20:43:07 +0900

    Add files via upload

---
+++ DownloadAndUnzip.sh
@@ -0,0 +1,6 @@
+wget https://data.airc.aist.go.jp/AutomaticLabeledLiDARData/HumanDetectionVer02.tar.gz
+wget https://data.airc.aist.go.jp/AutomaticLabeledLiDARData/TestsetForVer02.tar.gz
+
+tar -zxvf ./HumanDetectionVer02.tar.gz
+tar -zxvf ./TestsetForVer02.tar.gz
+
